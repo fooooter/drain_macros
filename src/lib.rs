10e105bde7535c693f81f2a008c254ecdda792d4
footer <0xb00b1e5@proton.me>
2024-12-18 19:45:46 +0100

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn drain_page(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item_str = item.to_string();
+    let page_fn_split = item_str.split_once('(').unwrap();
+    format!("{}(request_data: RequestData, response_headers: &mut HashMap<String, String>{}",
+            page_fn_split.0,
+            page_fn_split.1)
+        .parse()
+        .unwrap()
+}
+
+#[proc_macro]
+pub fn header(input: TokenStream) -> TokenStream {
+    let in_str = input.to_string();
+    let args_split = in_str.split_once(',').unwrap();
+    format!("response_headers.insert(String::from({}), String::from({}))",
+            args_split.0,
+            args_split.1)
+        .parse()
+        .unwrap()
+}

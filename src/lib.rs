a38efccea6fb2f364c8399066ad31ff35d36aca9
footer <0xb00b1e5@proton.me>
2024-12-19 19:22:38 +0100

    Implement cookies.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use proc_macro::TokenStream;
 pub fn drain_page(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let item_str = item.to_string();
     let page_fn_split = item_str.split_once('(').unwrap();
-    format!("{}(request_data: RequestData, response_headers: &mut HashMap<String, String>{}",
+    format!("{}(request_data: RequestData, response_headers: &mut HashMap<String, String>, set_cookie: &mut HashMap<String, SetCookie>{}",
             page_fn_split.0,
             page_fn_split.1)
         .parse()

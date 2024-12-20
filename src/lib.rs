ed67e0a7aec71da85b1741569ccfb73eced72ee7
footer <0xb00b1e5@proton.me>
2024-12-20 13:55:02 +0100

    Make the entire dynamic page asynchronous.

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,20 @@ use proc_macro::TokenStream;
 pub fn drain_page(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let item_str = item.to_string();
     let page_fn_split = item_str.split_once('(').unwrap();
-    format!("{}(request_data: RequestData, \
+    let page_fn_body = page_fn_split.1.split_once('{').unwrap().1;
+    format!("{}(request_data: drain_common::RequestData, \
                 request_headers: &HashMap<String, String>, \
                 response_headers: &mut HashMap<String, String>, \
-                set_cookie: &mut HashMap<String, SetCookie>{}",
-            page_fn_split.0,
-            page_fn_split.1)
+                set_cookie: &mut HashMap<String, drain_common::cookies::SetCookie>) -> Option<Vec<u8>> {{\
+                    tokio::runtime::Builder::new_multi_thread()\
+                        .enable_all()\
+                        .build()\
+                        .unwrap()\
+                        .block_on(async {{\
+                            {page_fn_body}\
+                        )\
+                }}",
+            page_fn_split.0)
         .parse()
         .unwrap()
 }

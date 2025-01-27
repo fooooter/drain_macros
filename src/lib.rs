62a2be13421858b4139e7c14c739196027937efd
footer <0xb00b1e5@proton.me>
2025-01-27 15:13:15 +0100

    Change "drain_page" to "drain_endpoint" and make specifying "export_name" unnecessary.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
 use proc_macro::TokenStream;
+use regex::Regex;
 
 #[proc_macro_attribute]
-pub fn drain_page(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn drain_endpoint(attr: TokenStream, item: TokenStream) -> TokenStream {
     let item_str = item.to_string();
+    let attr_str = attr.to_string();
+    let attr_str = attr_str.trim_matches('"');
+
     let page_fn_split = item_str.split_once('(').unwrap();
     let page_fn_body = page_fn_split.1.split_once('{').unwrap().1;
-    format!("{}(request_data: drain_common::RequestData, \
-                request_headers: &HashMap<String, String>, \
-                response_headers: &mut HashMap<String, String>, \
-                set_cookie: &mut HashMap<String, drain_common::cookies::SetCookie>) -> Option<Vec<u8>> {{\
+
+    let attr_regex = Regex::new(
+        r#"^((([A-Za-z0-9\-_]*\.[[:alnum:]]+/?)+)+|([A-Za-z0-9\-_]+/?)+)+$"#
+    ).unwrap();
+
+    if !attr_regex.is_match(&attr_str) {
+        panic!("Endpoint's name must consist only of alphanumeric characters, hyphens, underscores, slashes and dots.");
+    }
+
+    let attr_str_prepared = attr_str.trim_end_matches("/").replace("/", "::");
+
+    format!("#[export_name = \"{attr_str_prepared}\"]{}(request_data: drain_common::RequestData, \
+                request_headers: &std::collections::HashMap<String, String>, \
+                response_headers: &mut std::collections::HashMap<String, String>, \
+                set_cookie: &mut std::collections::HashMap<String, drain_common::cookies::SetCookie>) -> Option<Vec<u8>> {{\
                     tokio::runtime::Builder::new_multi_thread()\
                         .enable_all()\
                         .build()\
